3096f8ef4fd6d8bc1b787d906522be5238bdcf9c
rafael-lage <90780368+rafael-lage@users.noreply.github.com>
2023-10-05 16:29:32 +0000

    implementing grow_plant

--- src/main.cpp
+++ src/main.cpp
@@ -47,10 +47,19 @@ struct entity_t
     int32_t age;
 };
 
+int iteration = 1;
+
 //prototipos
 void initilize_entity (entity_t entity, int total_ent);
 bool isNoEntitity(pos_t pos);
-
+void simulate_ent(pos_t pos);
+void simulate_plant(pos_t pos);
+void simulate_herb(pos_t pos);
+void simulate_carn(pos_t pos);
+void cleanPosition(pos_t pos);
+bool isToGrow();
+pos_t chooseRandomPos(pos_t pos);
+void createEntity(entity_t entity, pos_t pos);
 
 // Function to generate a random action based on probability
 bool random_action(float probability) {
@@ -67,6 +76,18 @@ bool random_number() {
     return dis(gen);
 }
 
+int getRandomNumberFromVector(const std::vector<int>& numbers) {
+    if (numbers.empty()) {
+        // Handle case where the vector is empty
+        return -1; // You might want to choose a different default value
+    }
+
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, numbers.size() - 1);
+
+    return numbers[dis(gen)];
+}
 
 
 
@@ -129,8 +150,8 @@ int main()
         uint32_t initial_num_carn = (uint32_t)request_body["carnivores"];
 
         entity_t new_plant = {plant, 0, 0};
-        entity_t new_herb = {herbivore, 0, 0};
-        entity_t new_carn = {carnivore, 0, 0};
+        entity_t new_herb = {herbivore, 100, 0};
+        entity_t new_carn = {carnivore, 100, 0};
         
 
         initilize_entity(new_plant,initial_num_plants);
@@ -144,14 +165,24 @@ int main()
         res.body = json_grid.dump();
         res.end(); });
 
+
     // Endpoint to process HTTP GET requests for the next simulation iteration
     CROW_ROUTE(app, "/next-iteration")
         .methods("GET"_method)([]()
                                {
+        //int init_iteration = iteration;
+    pos_t current_pos;
         // Simulate the next iteration
         // Iterate over the entity grid and simulate the behaviour of each entity
-        
-        // <YOUR CODE HERE>
+        for(int i = 0;i<NUM_ROWS;i++){
+            for(int j = 0;j<NUM_ROWS;j++){
+                current_pos.i = i;
+                current_pos.j = j;
+                simulate_ent(current_pos);
+            }
+        }
+        //printf("%d\n",iteration);
+        iteration++;
         
         // Return the JSON representation of the entity grid
         nlohmann::json json_grid = entity_grid; 
@@ -160,6 +191,8 @@ int main()
 
     return 0;
 }
+
+
 
 
 void initilize_entity (entity_t entity, int total_ent){
@@ -186,4 +219,92 @@ bool isNoEntitity(pos_t pos){
     if(entity_grid[pos.i][pos.j].type != empty) return(false);
 
     else return(true);
+}
+
+void simulate_ent(pos_t pos){
+
+    if(entity_grid[pos.i][pos.j].type == plant)
+        simulate_plant(pos);
+
+    if(entity_grid[pos.i][pos.j].type == herbivore)
+        simulate_herb(pos);
+
+    if(entity_grid[pos.i][pos.j].type == carnivore)
+        simulate_carn(pos);
+        
+}
+
+void simulate_plant(pos_t pos){
+    //I am a plant
+    if(entity_grid[pos.i][pos.j].age == PLANT_MAXIMUM_AGE){
+        cleanPosition(pos);
+    }
+    
+    entity_grid[pos.i][pos.j].age +=1;
+}
+
+void simulate_herb(pos_t pos){
+    //I am a plant
+
+    if(isToGrow()){
+        pos_t new_plant_pos = chooseRandomPos(pos);
+        createEntity(entity_grid[pos.i][pos.j], new_plant_pos);
+    }
+
+    if(entity_grid[pos.i][pos.j].age == HERBIVORE_MAXIMUM_AGE){
+        cleanPosition(pos);
+    }
+    
+    entity_grid[pos.i][pos.j].age +=1;
+}
+
+void simulate_carn(pos_t pos){
+    //I am a plant
+    if(entity_grid[pos.i][pos.j].age == CARNIVORE_MAXIMUM_AGE){
+        cleanPosition(pos);
+    }
+    
+    entity_grid[pos.i][pos.j].age +=1;
+}
+
+
+void cleanPosition(pos_t pos){
+    entity_grid[pos.i][pos.j].type = empty;
+    entity_grid[pos.i][pos.j].age = 0;
+    entity_grid[pos.i][pos.j].energy = 0;
+}
+
+bool isToGrow(){
+    if(random_action(PLANT_REPRODUCTION_PROBABILITY)) return(true);
+    else return(false);
+}
+
+pos_t chooseRandomPos(pos_t pos){
+    std::vector<int> vec_pos_i;
+    std::vector<int> vec_pos_j;
+
+    for(int i = pos.i -1; i<= pos.i+1;i++){
+        for(int j = pos.j-1;j <= pos.j < pos.j+1;j++){
+            if(isNoEntitity(pos)){
+                vec_pos_i.push_back(i);
+                vec_pos_j.push_back(j);
+            }
+        }
+    }
+
+    int new_i = getRandomNumberFromVector(vec_pos_i);
+    int new_j = getRandomNumberFromVector(vec_pos_j);
+
+    pos_t new_pos;
+    pos.i = new_i;
+    pos.j = new_j;
+    
+    return(new_pos);
+}
+
+void createEntity(entity_t entity, pos_t pos){
+    entity_grid[pos.i][pos.j].type = entity.type;
+    entity_grid[pos.i][pos.j].age = 0;
+    entity_grid[pos.i][pos.j].energy = entity.energy;
+    
 }
